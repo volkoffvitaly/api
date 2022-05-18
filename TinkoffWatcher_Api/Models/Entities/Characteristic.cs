8a972fbf5ea85d63f724a19c550878ffce596d97
Polekh Pavel <pashmender@gmail.com>
2022-05-19 04:30:52 +0700

    fixes

--- TinkoffWatcher_Api/Models/Entities/Characteristic.cs
+++ TinkoffWatcher_Api/Models/Entities/Characteristic.cs
@@ -10,11 +10,11 @@ namespace TinkoffWatcher_Api.Models.Entities
     {
         public string Other { get; set; }
 
-        public Guid CharacteristicTypeId { get; set; }
+        public Guid? CharacteristicTypeId { get; set; }
         public virtual CharacteristicType CharacteristicType { get; set; }
-        public Guid CharacteristicValueId { get; set; }
+        public Guid? CharacteristicValueId { get; set; }
         public virtual CharacteristicValue CharacteristicValue { get; set; }
-        public Guid MarkId { get; set; }
+        public Guid? MarkId { get; set; }
         public virtual Mark Mark { get; set; }
     }
 }
