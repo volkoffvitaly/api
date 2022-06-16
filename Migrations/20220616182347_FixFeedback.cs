82684f220890bf1ee8eb6406ff4e5d37833ccb03
Polekh Pavel <pashmender@gmail.com>
2022-06-17 01:27:58 +0700

    Fix feedback column in db

---
+++ Migrations/20220616182347_FixFeedback.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TinkoffWatcher_Api.Migrations
+{
+    public partial class FixFeedback : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InterwiewId",
+                table: "Feedbacks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
